14395785f052afb3ef376fe223354f240c1a06fe
HassanDev13 <hassantupe1999@gmail.com>
2021-01-31 15:19:31 +0000

    retrofit practice

--- app/src/main/java/com/practice/implementretrofitbasicauthentication/MainActivity.kt
+++ app/src/main/java/com/practice/implementretrofitbasicauthentication/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.practice.implementretrofitbasicauthentication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+            setContentView(R.layout.activity_main)
+      //  var context: Context = this@MainActivity
+
+
     }
 }
